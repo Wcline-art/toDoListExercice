dff3f58ca964bc2d26faaae9ddba4848bfcc6b5b
Celine Worm <wormceline@gmail.com>
2019-09-21 19:01:07 +0200

    complete functions for input enter action button and create li in script file

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 let li;
 
+// FUNCTIONS//
+
 function getInputValueClick() {}
 function getInputValueEnter() {}
-function createElementLi() {}
+function createElementLi() {
+  li = document.createElement("li");
+}
 
-function addInputValueInLi() {}
+function addInputValueInLi() {
+  li.append(document.createTextNode(getFirstCapitalize()));
+}
 
-function insertNewLiInDom() {}
+function insertNewLiInDom() {
+  ul.insertAdjacentElement("beforeend", li);
+}
+
+function addItemInTodoList() {
+  createElementLi();
+  addInputValueInLi();
+  insertNewLiInDom();
+  deleteInputField();
+}

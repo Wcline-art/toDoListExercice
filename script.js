77898e8b56326ddfce87eac35f32b80d1304d6bb
Celine Worm <wormceline@gmail.com>
2019-09-21 19:45:49 +0200

    bug fixed at line 23

--- script.js
+++ script.js
@@ -16,27 +16,29 @@ function InputValue() {
 
 // function to get the value in the field when the user click to the button add
 function getInputValueClick() {
-    if (InputValue() > 0) {
-        addItemInTodoList();
-      }
+  if (InputValue() > 0) {
+    addItemInTodoList();
+  }
 }
 // function to get the value in the field when the user press  the button enter
 function getInputValueEnter(key) {
-    if (InputValue() > 0 && key.keyCode === 13) {
-        addItemInTodoList();
+  if (InputValue() > 0 && key.keyCode === 13) {
+    addItemInTodoList();
+  }
 }
+
 // function to change the first character of the string to uppercase and the other to lowercase
 function getFirstCapitalize() {
-    return (
-      input.value[0].toUpperCase() +
-      input.value.slice(1, InputValue()).toLowerCase()
-    );
-  }
+  return (
+    input.value[0].toUpperCase() +
+    input.value.slice(1, InputValue()).toLowerCase()
+  );
+}
 //   function to create new element 'li' in the the list
 function createElementLi() {
   li = document.createElement("li");
 }
-// function add value in the field 
+// function add value in the field
 function addInputValueInLi() {
   li.append(document.createTextNode(getFirstCapitalize()));
 }
@@ -47,8 +49,8 @@ function insertNewLiInDom() {
 
 // function delete value into the field after add in the list
 function deleteInputField() {
-    input.value = "";
-  }
+  input.value = "";
+}
 
 // function to add item in the list ( it's THE global function)
 function addItemInTodoList() {

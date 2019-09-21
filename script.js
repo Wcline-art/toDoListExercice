d679c0b3cad657546f4f35ef74ba12d4fcde4938
Celine Worm <wormceline@gmail.com>
2019-09-21 18:50:16 +0200

    create fuctions empty in script file

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
+// GLOBALS VARIABLES//
+
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 let li;
+
+function getInputValueClick() {}
+function getInputValueEnter() {}
+function createElementLi() {}
+
+function addInputValueInLi() {}
+
+function insertNewLiInDom() {}

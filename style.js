48d783641a820422a9ed0c0caa6ffb876e8c03d6
Celine Worm <wormceline@gmail.com>
2019-09-21 18:37:27 +0200

    initial commit

---
+++ style.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
+let li;
+
+function InputValue() {
+  return input.value.length;
+}
+function getInputValueClick() {
+  if (InputValue() > 0) {
+    addItemInTodoList();
+  }
+}
+function getInputValueEnter(key) {
+  if (InputValue() > 0 && key.keyCode === 13) {
+    addItemInTodoList();
+  }
+}
+
+function getFirstCapitalize() {
+  return (
+    input.value[0].toUpperCase() +
+    input.value.slice(1, InputValue()).toLowerCase()
+  );
+}
+
+function createElementLi() {
+  li = document.createElement("li");
+}
+
+function addInputValueInLi() {
+  li.append(document.createTextNode(getFirstCapitalize()));
+}
+
+function insertNewLiInDom() {
+  ul.insertAdjacentElement("beforeend", li);
+}
+
+function deleteInputField() {
+  input.value = "";
+}
+function addItemInTodoList() {
+  createElementLi();
+  addInputValueInLi();
+  insertNewLiInDom();
+  deleteInputField();
+}
+
+button.addEventListener("click", getInputValueClick);
+input.addEventListener("keypress", getInputValueEnter);
